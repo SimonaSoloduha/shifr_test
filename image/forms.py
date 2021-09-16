4edfb161cd3cb7b2a81b5421164cbf712924968d
Soloduha Simona <ls460simona@gmail.com>
2021-09-16 20:48:34 +0300

    add Validation

--- image/forms.py
+++ image/forms.py
@@ -7,6 +7,14 @@ class ImageForm(forms.ModelForm):
         model = Image
         fields = ('title', 'image')
 
+    def clean(self):
+        cleaned_data = super(ImageForm, self).clean()
+        image = cleaned_data.get('image')
+        image_format = image.content_type.split('/')[1]
+        if image_format not in ('jpeg', 'jpg'):
+            msg = 'Загрузите файл формата jpg'
+            self.add_error('image', msg)
+
 
 class HexForm(forms.Form):
     hex_number = forms.CharField(label='HEX number', max_length=6, min_length=6)
--- image/models.py
+++ image/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Image(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images')
+    title = models.CharField(max_length=200, verbose_name='Название изображения')
+    image = models.ImageField(upload_to='images', verbose_name='Изображение')
 
     def __str__(self):
         return self.title
